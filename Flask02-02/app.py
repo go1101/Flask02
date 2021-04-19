958ab58938bf0bf4f6d13f7eab1636e2546efd6f
Go Nishimoto <nishimoto.go@gmail.com>
2021-04-19 20:46:04 +0900

    Sourcetreeによる更新

--- Flask02-02/app.py
+++ Flask02-02/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/title/<title>')
-def title(title):
-    return render_template("index.html", title=title)
+@app.route('/search')
+def search():
+    q = request.args.get('q','')
+    return q
 
 
 if __name__ == "__main__":
